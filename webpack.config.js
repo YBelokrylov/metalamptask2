bcf4180c514b7270798ea5c5c52f46c68e8c5b6f
Your Name <you@example.com>
2021-08-10 12:43:44 +0300

    added webpack and git for package

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, 'pages'),
+  entry: {
+    'UI-Page': './UI-Page.js',
+  },
+  output: {
+    filename: './[name]/[name].js',
+    path: path.join(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        oneOf: [
+          {
+            use: [
+              {
+                loader: 'bemdecl-to-fs-loader',
+                options: { levels: ['commonBlocks'], extensions: ['css', 'js'] } // Add css and js files of BEM entities to bundle
+              },
+              { loader: 'html2bemdecl-loader' }, // convert HTML to bem DECL format
+              {
+                loader: 'pug-html-loader', // convert Pug to HTML
+                options: {
+                  // options to pass to the compiler same as: https://pugjs.org/api/reference.html
+                  data: {} // set of data to pass to the pug render.
+                }
+              }
+            ],
+            issuer: /\.js$/
+          },
+          {
+            use: [
+              {
+                loader: 'pug-loader',
+                options: {
+                  pretty: true
+                }
+              }
+            ]
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ]
+
+
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "./UI-Page/UI-Page.html",
+      template: './UI-Page.pug',
+      chunks: ['UI-Page']
+    })
+  ]
+}
